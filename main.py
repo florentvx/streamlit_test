db132f75075d988c5dbda0c743cae70a87a8e41f
florentvx <florent.vassaux@gmail.com>
2023-08-05 01:08:46 +0100

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from PIL import Image
+
+
+
+st.set_page_config(
+    page_title='My Data',
+    page_icon="🔵",
+    layout="wide"
+)
+
+st.header("my 2023 data")
+st.subheader("do i need a subheader? Really")
