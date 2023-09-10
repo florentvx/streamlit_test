6bd81eb05c37e63b2fef0a0acdeb76d1b88d03ac
florentvx <florent.vassaux@gmail.com>
2023-09-10 12:32:13 +0100

    pension tables

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-from PIL import Image
 import math
 
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+from PIL import Image
+
+import pension_calculator.src as pension
 
 st.set_page_config(
     page_title='My Data',
@@ -23,10 +25,26 @@ my_var = st.sidebar.multiselect(
     default=[option_ms[0], option_ms[-1]]
 )
 
-st.title(':pencil2: Numbers')
+st.title("💷 Tax Calculations")
+
+my_amount = 100000
+
+left_col, midd_col, right_col = st.columns([0.35,0.35,0.3])
+with left_col:
+    st.dataframe(pension.calculate_income_tax(my_amount))
+with midd_col:
+    st.dataframe(pension.calculate_national_insurance_tax(my_amount))
+with right_col:
+    st.dataframe(pension.calculate_all_taxes(my_amount))
+
+
+
+
+
 st.markdown("---")
 
-
+st.title(':pencil2:  Numbers')
+st.markdown("---")
 
 st.text(sum(map(float,my_var)))
 
@@ -64,6 +82,3 @@ footer {visibility: hidden;}
 </style>
 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-
-from .pension_calculator.src import calculate_net_income
-
