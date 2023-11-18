beee2c360810c91e2b97ad2794e3793089b2df2a
florentvx <florent.vassaux@gmail.com>
2023-11-18 12:08:31 +0000

    init inflation page

---
+++ pages/3_🎈_Inflation.py
@@ -0,0 +1,55 @@
+import datetime as dt
+import plotly.express as px
+
+from pension_simulator import convert_price_with_inflation
+
+from tools import *
+from tools.inflation import inflation_data
+
+set_page_config()
+
+st.title("🎈 Inflation")
+
+inflation_dict = {
+    row['Date']: row["UK CPIH Index"]
+    for _, row in inflation_data.iterrows()
+}
+
+cols_converter = st.columns([0.2]*5)
+with cols_converter[0]:
+    date_in = st.date_input("Input Date", dt.date(2020,1,1), min_value=dt.date(1989,1,1))
+with cols_converter[1]:
+    money_in = st.number_input("Amount of GBP", min_value=0.0, value=100.0)
+with cols_converter[3]:
+    date_out = st.date_input("Output Date", dt.date.today(), min_value=dt.date(1988,1,1))
+with cols_converter[4]:
+    st.write("Equivalent Amount")
+    money_out = st.text(f"£ {round(convert_price_with_inflation(inflation_dict, money_in, date_in, date_out),2)}")
+
+
+st.markdown('---')
+radio_choice = st.radio("Graph Choice: ", ["Index", "Rate"], index=1)
+
+my_line_chart = px.line(
+    inflation_data, 
+    x="Date", 
+    y="UK CPIH Index",
+    title="<b>UK CPIH</b>",
+    template="plotly",
+)
+
+my_second_line_chart = px.line(
+    inflation_data, 
+    x="Date", 
+    y="Inflation Rate (%)",
+    title="<b>Year On Year Inflation Rate</b>",
+    template="plotly",
+)
+
+left_col, midd_col, right_col = st.columns([0.25,0.5,0.25])
+if radio_choice == "Index":
+    midd_col.plotly_chart(my_line_chart, use_container_width=True)
+elif radio_choice == "Rate":
+    midd_col.plotly_chart(my_second_line_chart, use_container_width=True)
+else:
+    raise ValueError(f"radio choice not covered: {radio_choice}")
---
+++ tools/inflation.py
@@ -0,0 +1,20 @@
+import datetime as dt
+import pandas as pd
+
+from pension_simulator import MONTH_MAP
+
+
+def get_python_date(input: str):
+    month_input = input[5:]
+    month_input = month_input[0].upper() + month_input[1:].lower()
+    if month_input not in MONTH_MAP:
+        raise ValueError(f'{month_input} not in {MONTH_MAP.keys()}')
+    return dt.date(int(input[:4]), MONTH_MAP[month_input], 1)
+
+inflation_data = pd.read_csv(r'data/uk_cpih_timeseries.csv')
+inflation_data['Date'] = inflation_data['year_month_date'].apply(get_python_date)
+inflation_data = inflation_data.drop('year_month_date', axis = 1)
+
+inflation_data['UK CPIH Index'] = inflation_data['uk_cpih']
+inflation_data = inflation_data.drop("uk_cpih", axis=1)
+inflation_data['Inflation Rate (%)'] = round((inflation_data['UK CPIH Index'] / inflation_data['UK CPIH Index'].shift(12) - 1) * 100, 2)
